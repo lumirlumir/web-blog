875dbab0173a9aaf71951ae6949623d7aa81aa63
루밀LuMir <rpfos@naver.com>
2024-08-25 00:13:00 +0900

    feat: create layouts/Aside

---
+++ src/layouts/Aside/Aside.jsx
@@ -0,0 +1,68 @@
+import { promises as fs } from 'fs';
+import Link from 'next/link';
+
+import styles from './Aside.module.scss';
+
+export default async function Aside() {
+  return (
+    <aside className={styles.aside}>
+      <ul>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+      </ul>
+      {renderDirTree(buildDirTree(dirs))}
+    </aside>
+  );
+}
+
+const dirs = await fs.readdir(`${process.cwd()}/src/docs`, {
+  encoding: 'utf-8',
+  recursive: true,
+});
+
+function buildDirTree(dirs) {
+  const tree = {};
+
+  dirs.forEach(dir => {
+    const parts = dir.split(/[/\\]/); // Use `/` or `\\` as a path seperator.
+
+    let current = tree;
+
+    parts.forEach((part, index) => {
+      if (!current[part]) {
+        current[part] = index === parts.length - 1 ? null : {};
+      }
+      current = current[part];
+    });
+  });
+
+  return tree;
+}
+
+function renderDirTree(tree, basePath = '') {
+  return (
+    <ul>
+      {Object.keys(tree).map(key => {
+        const path = `${basePath}/${key}`;
+
+        if (tree[key] === null) {
+          return (
+            <li key={path}>
+              <Link href={`/docs${path.replace('.md', '')}`}>
+                {key.replace('.md', '')}
+              </Link>
+            </li>
+          );
+        } else {
+          return (
+            <li key={path}>
+              <span>{key}</span>
+              {renderDirTree(tree[key], path)}
+            </li>
+          );
+        }
+      })}
+    </ul>
+  );
+}
