2886419de79c008cd71562b15838f6edb08d8dd0
루밀LuMir <rpfos@naver.com>
2024-08-25 19:38:12 +0900

    fix: deployment error in vercel with generateStaticParams

--- src/app/docs/[...categories]/page.jsx
+++ src/app/docs/[...categories]/page.jsx
@@ -4,6 +4,19 @@ import parse from 'html-react-parser';
 import { REPOSITORY } from '@/constants/github';
 import Article from '@/layouts/Article';
 
+export async function generateStaticParams() {
+  const paths = await fs.readdir(`${process.cwd()}/src/docs`, {
+    encoding: 'utf-8',
+    recursive: true,
+  });
+
+  return paths
+    .filter(path => path.endsWith('.md'))
+    .map(path => ({
+      categories: path.replace(/\\/g, '/').replace('.md', '').split('/'),
+    }));
+}
+
 export default async function Page({ params }) {
   const markdown = await fs.readFile(
     `${process.cwd()}/src/docs/${params.categories.join('/')}.md`,
