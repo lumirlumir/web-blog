bb31a6e0acea4b6f25637f9be38bfd952919a01a
루밀LuMir <rpfos@naver.com>
2024-09-16 20:21:56 +0900

    refactor: move every grid related properties to main component

--- src/components/layouts/Main/Main.jsx
+++ src/components/layouts/Main/Main.jsx
@@ -1,5 +1,3 @@
-import styles from './Main.module.scss';
-
 export default function Main({ children }) {
-  return <main className={styles.main}>{children}</main>;
+  return <main>{children}</main>;
 }
