6fb69ad61db09b9f62ee4f8c0a98f784f153a3c3
루밀LuMir <rpfos@naver.com>
2024-09-17 13:42:19 +0900

    feat: add web analytics using @vercel/analytics (#64)
    web analytics 분석 도구 기능 도입.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import Article from '@/components/layouts/Article';
@@ -33,6 +34,7 @@ export default function RootLayout({ children }) {
           <Article>{children}</Article>
         </Main>
 
+        <Analytics />
         <SpeedInsights />
       </Body>
     </html>
