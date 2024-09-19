2c7578fac8b6410295e3f31eca3d50f45eba6a50
루밀LuMir <rpfos@naver.com>
2024-09-19 19:41:55 +0900

    feat: metadata (#67)
    * chore: install gray-matter as a dependency
    
    * feat: add USER.bio to constants/github.js
    
    * feat: add static metadata field to app/layout.jsx
    
    * feat: add gray-matter features to utils/mardownToJsx.js
    
    * feat: add dynamic metadata to app/docs/[...categories]/page.jsx

--- src/app/docs/[...categories]/page.jsx
+++ src/app/docs/[...categories]/page.jsx
@@ -2,7 +2,16 @@ import { promises as fs } from 'fs';
 import { join, sep } from 'path';
 
 import { DOCS } from '@/constants/path';
-import markdownToJsx from '@/utils/markdownToJsx';
+import markdownToJsx, { readMarkdownWithFrontMatter } from '@/utils/markdownToJsx';
+
+/* Custom Declaration */
+function getFilePath(params) {
+  return join(DOCS, `${params.categories.join(sep)}.md`);
+}
+
+/* Next.js Declaration */
+// Control what happens when a dynamic segment is visited that was not generated with `generateStaticParams`.
+export const dynamicParams = false;
 
 export async function generateStaticParams() {
   const paths = await fs.readdir(DOCS, {
@@ -16,8 +25,17 @@ export async function generateStaticParams() {
     }));
 }
 
-export default async function Page({ params }) {
-  const filePath = join(DOCS, `${params.categories.join(sep)}.md`);
+export async function generateMetadata({ params }) {
+  const {
+    data: { title, description },
+  } = await readMarkdownWithFrontMatter(getFilePath(params));
 
-  return <>{await markdownToJsx(filePath)}</>;
+  return {
+    title,
+    description,
+  };
+}
+
+export default async function Page({ params }) {
+  return <>{await markdownToJsx(getFilePath(params))}</>;
 }
