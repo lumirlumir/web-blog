7b0229203518908c3a4c981003cf35f63400b1e3
루밀LuMir <rpfos@naver.com>
2024-10-06 15:15:53 +0900

    refactor: create utils/fs.js

---
+++ src/utils/fs.js
@@ -0,0 +1,36 @@
+import { promises as fs } from 'fs';
+
+import matter from 'gray-matter';
+
+/**
+ * Reads the contents of a file.
+ *
+ * @async
+ * @param {string} filePath The path to the file.
+ * @returns {Promise<string>} The content of the file.
+ */
+export async function readFile(filePath) {
+  return fs.readFile(filePath, 'utf-8');
+}
+
+/**
+ * Reads a Markdown file and returns either the content or data(front matter).
+ *
+ * @async
+ * @param {string} filePath The path to the Markdown file.
+ * @param {'content' | 'data'} [option='content'] The type of data to return.
+ * @returns {Promise<string | {[key: string]: any}>} The content or data(front matter) of the file.
+ * @throws {TypeError} If the option is invalid.
+ */
+export async function readMarkdown(filePath, option = 'content') {
+  const { content, data } = matter(await readFile(filePath));
+
+  switch (option) {
+    case 'content':
+      return content;
+    case 'data':
+      return data;
+    default:
+      throw TypeError();
+  }
+}
