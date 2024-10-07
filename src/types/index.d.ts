7043e89e8d79b1a858f1f6a3e8a3f5235b8f49cd
루밀LuMir <rpfos@naver.com>
2024-10-07 23:01:44 +0900

    refactor: create tagTree.js

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,26 @@
 /**
+ * Represents a Markdown file structure with its content and data(front matter).
+ */
+export type Markdown = {
+  content: string;
+  data: {
+    [key: string]: any;
+  };
+};
+
+/**
  * Represents a node in a directory tree.
  */
 export type DirTreeNode = {
-  // The name of the node.
-  name: string;
-  // The array of child nodes if the node is a directory. This is a recursive structure.
-  children?: DirTreeNode[];
+  name: string; // The name of the node.
+  children?: DirTreeNode[]; // The array of child nodes if the node is a directory. This is a recursive structure.
+};
+
+/**
+ * Represents a node in a tag tree.
+ */
+export type TagTreeNode = {
+  [key: string]: {
+    basename: string;
+  } & Markdown;
 };
