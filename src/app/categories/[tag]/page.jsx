7a6fabad7e65bd669959dd2832ba9d833f0d448f
루밀LuMir <rpfos@naver.com>
2024-10-08 23:04:57 +0900

    feat: add react-icons in categories/[tag]/page.jsx

--- src/app/categories/[tag]/page.jsx
+++ src/app/categories/[tag]/page.jsx
@@ -1,5 +1,8 @@
 import Link from 'next/link';
 
+import { Fragment } from 'react';
+import { FaBook, FaTag, FaRegCalendarPlus, FaRegCalendarXmark } from 'react-icons/fa6';
+
 import { DOCS, EXTENSION } from '@/constants/path';
 import { readMarkdownTagTree } from '@/utils/fs';
 import { markdownToText } from '@/utils/markup';
@@ -25,10 +28,31 @@ export default async function Page({ params }) {
             {markdownToText(title)}
           </Link>
         </h2>
-        <p>{markdownToText(description)}</p>
-        <p>{tags.join(', ')}</p>
         <p>
-          『만든 날짜: {created}』 『수정한 날짜: {updated}』
+          <FaBook />
+          {markdownToText(description)}
+        </p>
+        <p>
+          {tags.map(tag => (
+            <Fragment key={tag}>
+              <FaTag />
+              <span>{tag}</span>
+              &ensp;
+            </Fragment>
+          ))}
+        </p>
+        <p>
+          <span>
+            <FaRegCalendarPlus />
+            <sup>created</sup>
+            {created}
+            &ensp;
+          </span>
+          <span>
+            <FaRegCalendarXmark />
+            <sup>updated</sup>
+            {updated}
+          </span>
         </p>
         <hr />
       </div>
