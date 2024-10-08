65b7134d3b0f05d3f337341ed253437fafc4d6ee
루밀LuMir <rpfos@naver.com>
2024-10-08 22:33:38 +0900

    feat: make date visible in categories/[tag]/page.jsx

--- src/app/categories/[tag]/page.jsx
+++ src/app/categories/[tag]/page.jsx
@@ -9,16 +9,29 @@ const { mdRegExp } = EXTENSION;
 export default async function Page({ params }) {
   const tagTree = await readMarkdownTagTree(DOCS);
 
-  return tagTree[params.tag].map(({ basename, data: { title, description, tags } }) => (
-    <div key={basename}>
-      <h2>
-        <Link href={`/posts/${basename.replace(mdRegExp, '')}`}>
-          {markdownToText(title)}
-        </Link>
-      </h2>
-      <p>{markdownToText(description)}</p>
-      <p>{tags.join(', ')}</p>
-      <hr />
-    </div>
-  ));
+  return tagTree[params.tag].map(
+    ({
+      basename,
+      data: {
+        title,
+        description,
+        tags,
+        date: { created, updated },
+      },
+    }) => (
+      <div key={basename}>
+        <h2>
+          <Link href={`/posts/${basename.replace(mdRegExp, '')}`}>
+            {markdownToText(title)}
+          </Link>
+        </h2>
+        <p>{markdownToText(description)}</p>
+        <p>{tags.join(', ')}</p>
+        <p>
+          『만든 날짜: {created}』 『수정한 날짜: {updated}』
+        </p>
+        <hr />
+      </div>
+    ),
+  );
 }
