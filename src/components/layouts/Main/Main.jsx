6de73c000e6d9b82ebd8ac54f26e4ea8f1489b2d
루밀LuMir <rpfos@naver.com>
2024-10-09 12:55:09 +0900

    design: make Main component using grid to separate article and nav

--- src/components/layouts/Main/Main.jsx
+++ src/components/layouts/Main/Main.jsx
@@ -1,3 +1,5 @@
+import styles from './Main.module.scss';
+
 export default function Main({ children }) {
-  return <main>{children}</main>;
+  return <main className={styles.main}>{children}</main>;
 }
