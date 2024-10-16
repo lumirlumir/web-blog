f9d837f686c4d1a5f090541f1d08af44667c59de
루밀LuMir <rpfos@naver.com>
2024-10-16 17:00:42 +0900

    feat: make [tag]/page.jsx use generateStaticParams

--- src/app/categories/[tag]/page.jsx
+++ src/app/categories/[tag]/page.jsx
@@ -8,9 +8,19 @@ import { compareMarkdownDocument } from '@/utils/compare';
 import { readMarkdownTagTree } from '@/utils/fs';
 import { markdownToJsx } from '@/utils/markup';
 
+/* Next.js Declaration */
+// Control what happens when a dynamic segment is visited that was not generated with `generateStaticParams`.
+export const dynamicParams = false;
+
+export async function generateStaticParams() {
+  const tagTree = await readMarkdownTagTree(PATH_DOCS);
+  const tags = Object.keys(tagTree);
+
+  return tags.map(tag => ({ tag }));
+}
+
 export default async function Page({ params, searchParams }) {
   const { sort = 'updated', order = 'desc' } = searchParams;
-
   const tagTree = await readMarkdownTagTree(PATH_DOCS);
 
   return (
