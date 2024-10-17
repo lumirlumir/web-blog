eaee0bfe1b3114e39519a631e5c9a2649899b90d
루밀LuMir <rpfos@naver.com>
2024-10-18 00:02:16 +0900

    design: update `Loading` component and others accordingly

--- src/components/common/Loading/Loading.jsx
+++ src/components/common/Loading/Loading.jsx
@@ -6,13 +6,15 @@ export default function Loading({ content }) {
   return (
     <div className={styles.loading}>
       <div>
-        <Image
-          src={'/images/loading.gif'}
-          width={48}
-          height={48}
-          alt="GitHub gif loading image"
-        />
-        <div>{content} 불러오는 중...</div>
+        <div>
+          <Image
+            src={'/images/loading.gif'}
+            width={48}
+            height={48}
+            alt="GitHub GIF loading image"
+          />
+        </div>
+        <div className={styles.content}>{content} 불러오는 중...</div>
       </div>
     </div>
   );
