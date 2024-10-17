1b267c5019d8ea28f1d20b3aed91ed32c7a97f09
루밀LuMir <rpfos@naver.com>
2024-10-17 20:06:47 +0900

    refactor: detach markdown related css into `_markdown.scss` and create `_base.scss`

--- src/components/layouts/Body/Body.jsx
+++ src/components/layouts/Body/Body.jsx
@@ -1,5 +1,5 @@
 import styles from './Body.module.scss';
 
 export default function Body({ children }) {
-  return <body className={`markdown-body ${styles.body}`}>{children}</body>;
+  return <body className={styles.body}>{children}</body>;
 }
