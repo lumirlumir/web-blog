6637bd3569402a2d5324f8277c67129fe8b795d0
루밀LuMir <rpfos@naver.com>
2024-10-17 15:45:48 +0900

    refactor: create `_variables.scss` and update code accordingly

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
     includePaths: [path.resolve(__dirname, 'src', 'styles')],
     additionalData: `
       @import 'utils/mixins';
+      @import 'utils/variables';
     `,
   },
   // Remove `console.*` output except `console.error`
