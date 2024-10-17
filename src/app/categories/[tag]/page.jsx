fcf6dc961c9d27e87605e033194821f90cb9988c
루밀LuMir <rpfos@naver.com>
2024-10-18 00:19:02 +0900

    revert: separate `Suspense` from `[tag]/page.jsx` and create a `loading.jsx` file.
    This reverts commit 9f056aec05e4db4d6959f13607ed6e1aa702b6f7.

--- src/app/categories/[tag]/page.jsx
+++ src/app/categories/[tag]/page.jsx
@@ -1,7 +1,9 @@
 import Link from 'next/link';
 
-import { Fragment } from 'react';
+import { Fragment, Suspense } from 'react';
 import { FaBook, FaTag, FaRegCalendarPlus, FaRegCalendarXmark } from 'react-icons/fa6';
+
+import Loading from '@/components/common/Loading';
 
 import { PATH_DOCS, EXT_MD_REGEXP } from '@/constants';
 import { compareMarkdownDocument } from '@/utils/compare';
@@ -23,42 +25,46 @@ export default async function Page({ params, searchParams }) {
   const { sort = 'updated', order = 'desc' } = searchParams;
   const tagTree = await readMarkdownTagTree(PATH_DOCS);
 
-  return tagTree[params.tag]
-    .sort(compareMarkdownDocument(sort, order))
-    .map(({ basename, data: { title, description, created, updated, tags } }) => (
-      <div key={basename}>
-        <h2>
-          <Link href={`/posts/${basename.replace(EXT_MD_REGEXP, '')}`}>
-            {markdownToJsx(title)}
-          </Link>
-        </h2>
-        <p>
-          <FaBook />
-          {markdownToJsx(description)}
-        </p>
-        <p>
-          {tags.map(tag => (
-            <Fragment key={tag}>
-              <FaTag />
-              <span>{tag}</span>
-              &ensp;
-            </Fragment>
-          ))}
-        </p>
-        <p>
-          <span>
-            <FaRegCalendarPlus />
-            <sup>created</sup>
-            {created}
-            &ensp;
-          </span>
-          <span>
-            <FaRegCalendarXmark />
-            <sup>updated</sup>
-            {updated}
-          </span>
-        </p>
-        <hr />
-      </div>
-    ));
+  return (
+    <Suspense key={sort + order} fallback={<Loading content="목록" />}>
+      {tagTree[params.tag]
+        .sort(compareMarkdownDocument(sort, order))
+        .map(({ basename, data: { title, description, created, updated, tags } }) => (
+          <div key={basename}>
+            <h2>
+              <Link href={`/posts/${basename.replace(EXT_MD_REGEXP, '')}`}>
+                {markdownToJsx(title)}
+              </Link>
+            </h2>
+            <p>
+              <FaBook />
+              {markdownToJsx(description)}
+            </p>
+            <p>
+              {tags.map(tag => (
+                <Fragment key={tag}>
+                  <FaTag />
+                  <span>{tag}</span>
+                  &ensp;
+                </Fragment>
+              ))}
+            </p>
+            <p>
+              <span>
+                <FaRegCalendarPlus />
+                <sup>created</sup>
+                {created}
+                &ensp;
+              </span>
+              <span>
+                <FaRegCalendarXmark />
+                <sup>updated</sup>
+                {updated}
+              </span>
+            </p>
+            <hr />
+          </div>
+        ))}
+    </Suspense>
+  );
 }
--- src/app/categories/loading.jsx
+++
@@ -1,5 +0,0 @@
-import LoadingOriginal from '@/components/common/Loading';
-
-export default function Loading() {
-  return <LoadingOriginal content="목록" />;
-}
