9182015265c52fabd8c1084cf58c86d79edde707
루밀LuMir <rpfos@naver.com>
2024-10-18 18:50:37 +0900

    design: update `component/header/Title` component

--- src/components/header/Title/Title.jsx
+++ src/components/header/Title/Title.jsx
@@ -1,7 +1,27 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
-import { GITHUB_USER_NAME, GITHUB_USER_HTML_URL } from '@/constants';
+import { getGithubUsers } from '@/utils/fetch';
 
-export default function Title() {
-  return <Link href={GITHUB_USER_HTML_URL}>{GITHUB_USER_NAME}</Link>;
+import styles from './Title.module.scss';
+
+export default async function Title() {
+  const { avatar_url, bio, name } = await getGithubUsers();
+
+  return (
+    <div className={styles.title}>
+      <Image
+        src={avatar_url}
+        width={36}
+        height={36}
+        alt={`${name}'s GitHub profile image`}
+      />
+      <div>
+        <div className={styles['user-name']}>
+          <Link href="/">{name}</Link>
+        </div>
+        <div className={styles['user-bio']}>{bio}</div>
+      </div>
+    </div>
+  );
 }
