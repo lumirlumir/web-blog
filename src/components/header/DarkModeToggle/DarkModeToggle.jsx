fe1e439a3677db624fde9bb078edcba737baa51c
루밀LuMir <rpfos@naver.com>
2024-10-21 20:55:54 +0900

    fix: icon theme in the `DarkModeToggle` component is out of sync on page refresh.

--- src/components/header/DarkModeToggle/DarkModeToggle.jsx
+++ src/components/header/DarkModeToggle/DarkModeToggle.jsx
@@ -10,25 +10,27 @@ export default function DarkModeToggle() {
 
   return (
     <div className={styles['dark-mode-toggle']}>
-      <button
-        className={`${styles['mode-switcher']} ${theme === 'dark' ? '' : styles.active}`}
-        onClick={toggleTheme}
-      >
-        <span className={styles['mode-switcher-main-body']}></span>
-        <span className={styles['mode-switcher-shadow-shape']}></span>
-        <span
-          className={`${styles['mode-switcher-sunray']} ${styles['mode-switcher-sunray-sunray-1']}`}
-        ></span>
-        <span
-          className={`${styles['mode-switcher-sunray']} ${styles['mode-switcher-sunray-sunray-2']}`}
-        ></span>
-        <span
-          className={`${styles['mode-switcher-sunray']} ${styles['mode-switcher-sunray-sunray-3']}`}
-        ></span>
-        <span
-          className={`${styles['mode-switcher-sunray']} ${styles['mode-switcher-sunray-sunray-4']}`}
-        ></span>
-      </button>
+      {theme && (
+        <button
+          className={`${styles['mode-switcher']} ${theme === 'dark' ? '' : styles.active}`}
+          onClick={toggleTheme}
+        >
+          <span className={styles['mode-switcher-main-body']}></span>
+          <span className={styles['mode-switcher-shadow-shape']}></span>
+          <span
+            className={`${styles['mode-switcher-sunray']} ${styles['mode-switcher-sunray-sunray-1']}`}
+          ></span>
+          <span
+            className={`${styles['mode-switcher-sunray']} ${styles['mode-switcher-sunray-sunray-2']}`}
+          ></span>
+          <span
+            className={`${styles['mode-switcher-sunray']} ${styles['mode-switcher-sunray-sunray-3']}`}
+          ></span>
+          <span
+            className={`${styles['mode-switcher-sunray']} ${styles['mode-switcher-sunray-sunray-4']}`}
+          ></span>
+        </button>
+      )}
     </div>
   );
 }
