b18520aff13a61111107d89f89680682cd5d036e
루밀LuMir <rpfos@naver.com>
2024-10-21 14:16:15 +0900

    chore: update `next.config.js`
    `domains` is deprecated. So I replaced it with `remotePatterns`.
    
    I added `isProduction` variable to determine whether it is `development` or `production`. And I applied it to `compiler` option to remove `console` logs only when it is in `production` mode not in `development` or `test`.

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 const path = require('path');
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   images: {
-    domains: ['avatars.githubusercontent.com'], // Allow GitHub profile image.
+    remotePatterns: [
+      {
+        hostname: 'avatars.githubusercontent.com', // Allow GitHub profile image.
+      },
+    ],
   },
   sassOptions: {
     includePaths: [path.resolve(__dirname, 'src', 'styles')],
@@ -11,10 +16,12 @@ module.exports = {
       @import 'utils/variables';
     `,
   },
-  // Remove `console.*` output except `console.error`
-  compiler: {
-    removeConsole: {
-      exclude: ['error'],
+  // Remove `console.*` output except `console.warn` and `console.error` only in production.
+  ...(isProduction && {
+    compiler: {
+      removeConsole: {
+        exclude: ['warn', 'error'],
+      },
     },
-  },
+  }),
 };
