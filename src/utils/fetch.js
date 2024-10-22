00b72f56857a9cbb2de7b00d6cd1b32098671804
루밀LuMir <rpfos@naver.com>
2024-10-22 19:35:27 +0900

    refactor: add `Authorization` header to `getGithubUsers` function

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -7,7 +7,11 @@ import { GITHUB_USER_LOGIN } from '@/constants';
  * @returns {Promise<Object>} A promise that resolves to a GitHub user data object.
  */
 export async function getGithubUsers() {
-  const response = await fetch(`https://api.github.com/users/${GITHUB_USER_LOGIN}`);
+  const response = await fetch(`https://api.github.com/users/${GITHUB_USER_LOGIN}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.GH_PAT}`,
+    },
+  });
 
   return response.json();
 }
