350b86686ee5011efd68f94d08f3d1a27705abd0
루밀LuMir <rpfos@naver.com>
2024-10-24 10:46:32 +0900

    feat: create `Sort` component in Nav

--- src/components/nav/SortContainer/SortContainer.jsx
+++ src/components/nav/SortContainer/SortContainer.jsx
@@ -1,12 +1,42 @@
 'use client';
 
+import { useState } from 'react';
 import { usePathname, useSearchParams, useRouter } from 'next/navigation';
-import { FaArrowDownWideShort, FaArrowUpShortWide } from 'react-icons/fa6';
+import {
+  FaAngleDown,
+  FaAngleUp,
+  FaArrowDownWideShort,
+  FaArrowUpShortWide,
+} from 'react-icons/fa6';
 import { GrSort } from 'react-icons/gr';
 
 import { MARKDOWN_DOCUMENT_DATA_META } from '@/data';
 
-export default function SortContainer() {
+import styles from './Sort.module.scss';
+
+function SortContainer({ children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function onClick() {
+    setIsOpen(prevState => !prevState);
+  }
+
+  return (
+    <div>
+      <div className={styles['sort-item']} onClick={onClick}>
+        <div className={styles['react-icons']}>
+          <GrSort />
+        </div>
+        <div className={styles['name-en']}>Sort</div>
+        <div className={styles['name-ko']}>정렬</div>
+        <div className={styles['order']}>{isOpen ? <FaAngleUp /> : <FaAngleDown />}</div>
+      </div>
+      {isOpen && <ul>{children}</ul>}
+    </div>
+  );
+}
+
+function SortItem({ sort, order }) {
   const pathname = usePathname();
   const searchParams = useSearchParams();
   const { replace } = useRouter();
@@ -21,64 +51,35 @@ export default function SortContainer() {
   }
 
   return (
-    <div>
-      <h2>
-        <GrSort size="16px" /> 정렬
-      </h2>
-      {/* TODO: Make a reusable component named `SortItem.jsx` */}
-      <div>
-        <div>
-          {MARKDOWN_DOCUMENT_DATA_META.title.reactIcons}
-          <sup>
-            {MARKDOWN_DOCUMENT_DATA_META.title.name.en}
-            {MARKDOWN_DOCUMENT_DATA_META.title.name.ko}
-          </sup>
-        </div>
-        <div>
-          <button onClick={() => onClick('title', 'desc')}>
-            <FaArrowDownWideShort size="16px" title="내림차순" />
-          </button>
-          <button onClick={() => onClick('title', 'asc')}>
-            <FaArrowUpShortWide size="16px" title="오름차순" />
-          </button>
-        </div>
+    <li className={styles['sort-item']} onClick={() => onClick(sort, order)}>
+      <div className={styles['react-icons']}>
+        {MARKDOWN_DOCUMENT_DATA_META[sort].reactIcons}
       </div>
-      <div>
-        <div>
-          {MARKDOWN_DOCUMENT_DATA_META.created.reactIcons}
-          <sup>
-            {MARKDOWN_DOCUMENT_DATA_META.created.name.en}
-            {MARKDOWN_DOCUMENT_DATA_META.created.name.ko}
-          </sup>
-        </div>
-        <div>
-          <button onClick={() => onClick('created', 'desc')}>
-            <FaArrowDownWideShort size="16px" title="내림차순" />
-          </button>
-          <button onClick={() => onClick('created', 'asc')}>
-            <FaArrowUpShortWide size="16px" title="오름차순" />
-          </button>
-        </div>
+      <div
+        className={styles['name-en']}
+      >{`${MARKDOWN_DOCUMENT_DATA_META[sort].name.en} / ${order === 'desc' ? 'Desc' : 'Asc'}`}</div>
+      <div
+        className={styles['name-ko']}
+      >{`${MARKDOWN_DOCUMENT_DATA_META[sort].name.ko} / ${order === 'desc' ? '내림차순' : '오름차순'}`}</div>
+      <div className={styles['order']}>
+        {order === 'desc' ? <FaArrowDownWideShort /> : <FaArrowUpShortWide />}
       </div>
-      <div>
-        <div>
-          {MARKDOWN_DOCUMENT_DATA_META.updated.reactIcons}
-          <sup>
-            {MARKDOWN_DOCUMENT_DATA_META.updated.name.en}
-            {MARKDOWN_DOCUMENT_DATA_META.updated.name.ko}
-          </sup>
-        </div>
-        <div>
-          <button onClick={() => onClick('updated', 'desc')}>
-            <FaArrowDownWideShort size="16px" title="내림차순" />
-          </button>
-          <button onClick={() => onClick('updated', 'asc')}>
-            <FaArrowUpShortWide size="16px" title="오름차순" />
-          </button>
-        </div>
-      </div>
-    </div>
+    </li>
+  );
+}
+
+export default function Sort() {
+  return (
+    <SortContainer>
+      <SortItem sort="title" order="desc" />
+      <SortItem sort="title" order="asc" />
+      <SortItem sort="created" order="desc" />
+      <SortItem sort="created" order="asc" />
+      <SortItem sort="updated" order="desc" />
+      <SortItem sort="updated" order="asc" />
+    </SortContainer>
   );
 }
 
 // TODO: useCallback hook
+// TODO: add `title` prop for a11y
